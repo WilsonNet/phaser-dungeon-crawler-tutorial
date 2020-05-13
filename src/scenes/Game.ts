e4285855b836e5eff22a5627e36828bc5a50fd5f
Wilson Neto <wilsonpsn@hotmail.com>
2020-05-13 16:11:48 -0300

    Walls collidable and debuggable

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,19 +1,28 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
-export default class HelloWorldScene extends Phaser.Scene
-{
-	constructor()
-	{
-		super('game')
-	}
+export default class HelloWorldScene extends Phaser.Scene {
+  constructor() {
+    super('game');
+  }
 
-	preload()
-    {
+  preload() {}
 
-    }
+  create() {
+    // key = this.load.tilemapTiledJSON
+    const map = this.make.tilemap({ key: 'dungeon' });
+    // name = JSON tilesets.name, key = this.load.image
+    const tileset = map.addTilesetImage('dungeon', 'tiles');
+    // Nome das Layers no TIled
+    map.createStaticLayer('Ground', tileset);
+    const wallsLayer = map.createStaticLayer('Walls', tileset);
 
-    create()
-    {
-        this.add.image(0,0, 'tiles')
-    }
+    wallsLayer.setCollisionByProperty({ collides: true });
+
+    const debugGraphics = this.add.graphics().setAlpha(0.7);
+    wallsLayer.renderDebug(debugGraphics, {
+      tileColor: null,
+      collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255),
+    });
+  }
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,14 +1,14 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
 export default class Preloader extends Phaser.Scene {
   constructor() {
-    super('preloader')
+    super('preloader');
   }
-  preload(){
-    this.load.image('tiles', 'tiles/dungeon_tiles.png')
+  preload() {
+    this.load.image('tiles', 'tiles/dungeon_tiles.png');
+    this.load.tilemapTiledJSON('dungeon', 'tiles/dungeon-01.json');
   }
-  create(){
-    this.scene.start('game')
+  create() {
+    this.scene.start('game');
   }
-
 }
