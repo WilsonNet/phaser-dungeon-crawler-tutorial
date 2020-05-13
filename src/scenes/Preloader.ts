d9e67dda6e4ee7ca563fa7ad192b9dc3240ded09
Wilson Neto <wilsonpsn@hotmail.com>
2020-05-13 15:58:17 -0300

    Image is now loading

---
+++ src/scenes/Preloader.ts
@@ -0,0 +1,14 @@
+import Phaser from 'phaser'
+
+export default class Preloader extends Phaser.Scene {
+  constructor() {
+    super('preloader')
+  }
+  preload(){
+    this.load.image('tiles', 'tiles/dungeon_tiles.png')
+  }
+  create(){
+    this.scene.start('game')
+  }
+
+}
